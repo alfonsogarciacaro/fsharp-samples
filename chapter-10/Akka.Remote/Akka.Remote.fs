15f8afdc6d99410641ec3f05852ddf067a37ce2e
Alfonso García-Caro <alfonso.garcia-caro@hotmail.com>
2016-10-03 13:45:16 +0200

    Add App.config

--- chapter-10/Akka.Remote/Akka.Remote.fs
+++ chapter-10/Akka.Remote/Akka.Remote.fs
@@ -4,18 +4,23 @@ open Akka.FSharp
 open Akka.Actor
 open System
 
-let config =  
-    Configuration.parse
-        @"akka {
-            actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
-            remote.helios.tcp {
-                hostname = localhost
-                port = 9001
-            }
-        }"
+let config = """
+akka {  
+    actor {
+        provider = "Akka.Remote.RemoteActorRefProvider, Akka.Remote"
+    }    
+    remote.helios.tcp {
+        transport-protocol = tcp
+        port = 9001                 
+        hostname = localhost
+    }
+}
+"""
 
 [<EntryPoint>]
 let main args =
-    use system = System.create "remote-system" config
+    use system =
+        Configuration.parse config
+        |> System.create "remote-system"
     Console.ReadLine() |> ignore
     0
