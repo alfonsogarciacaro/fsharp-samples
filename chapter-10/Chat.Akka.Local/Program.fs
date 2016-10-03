d4bdd1a223923ff84869791bd45d95284363f70d
Alfonso García-Caro <alfonso.garcia-caro@hotmail.com>
2016-10-03 19:26:45 +0200

    Fix Chat.Akka.Remote sample

--- chapter-10/Chat.Akka.Local/Program.fs
+++ chapter-10/Chat.Akka.Local/Program.fs
@@ -10,41 +10,61 @@ let config =
         @"akka {
             actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
             remote.helios.tcp {
-                hostname = localhost
-                port = 9001
+                transport-protocol = tcp
+                hostname = 0.0.0.0
+                port = 0 # get first available port
             }
         }"
 
-// return Deploy instance able to operate in remote scope
-let deployRemotely =
-    Address.Parse >> RemoteScope >> Deploy
+// create remote deployment configuration for actor system available under `actorPath`
+let remoteDeploy systemPath = 
+    let address = 
+        match ActorPath.TryParseAddress systemPath with
+        | false, _ -> failwith "ActorPath address cannot be parsed"
+        | true, a -> a
+    Deploy(RemoteScope(address))
 
-let spawnRemote systemOrContext remoteSystemAddress actorName expr = 
-    deployRemotely remoteSystemAddress
-    |> SpawnOption.Deploy
-    |> List.singleton
-    |> spawne systemOrContext actorName expr
+let [<Literal>] REQ = 1
+let [<Literal>] RES = 2
 
 [<EntryPoint>]
-let main args =
+let main _args =
+    System.Console.Title <- "Local: " + System.Diagnostics.Process.GetCurrentProcess().Id.ToString()
+    // remote system address according to settings provided 
+    // in FSharp.Deploy.Remote configuration
+    let remoteSystemAddress = "akka.tcp://remote-system@192.168.2.101:7000"
     let system = System.create "local-system" config
     
     let aref =  
-        spawnRemote system "akka.tcp://remote-system@localhost:9001/" "hello"
-            // actorOf wraps custom handling function with message receiver logic
-            <@ actorOf (fun msg -> printfn "received '%s'" msg) @>
+        // as long as actor receive logic is serializable F# Expr, there is no need for sharing any assemblies 
+        // all code will be serialized, deployed to remote system and there compiled and executed
+        spawne system "remote" 
+            <@ 
+                fun mailbox -> 
+                let rec loop(): Cont<string, unit> = 
+                    actor { 
+                        let! msg = mailbox.Receive()
+                        printfn "Remote actor received: %s" msg
+                        return! loop()
+                    }
+                loop() 
+             @> [ SpawnOption.Deploy(remoteDeploy remoteSystemAddress) ]
+
+//    async { 
+//        let! msg = aref <? (REQ, "hello")
+//        match msg with
+//        | (RES, m) -> printfn "Remote actor responded: %s" m
+//        | _ -> printfn "Unexpected response from remote actor"
+//    }
+//    |> Async.RunSynchronously
 
     // send example message to remotely deployed actor
     aref <! "Hello world"
-
-    // thanks to location transparency, we can select 
-    // remote actors as if they where existing on local node
-    let sref = select "akka://local-system/user/hello" system  
-    sref <! "Hello again"
 
     // we can still create actors in local system context
     let lref = spawn system "local" (actorOf (fun msg -> printfn "local '%s'" msg))  
     // this message should be printed in local application console
     lref <! "Hello locally"
 
+    System.Console.ReadLine() |> ignore
     0
